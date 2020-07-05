e60ce2332a379cb7c71a0a947968a3850b8b44b4
arjohn <arjohnqafsignal@gmail.com>
2020-07-05 10:28:17 +0300

    update auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,19 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function googleLogin(Request $request)
+    public function loginGoogle(Request $request)
     {
         $request->validate([
             'name' => 'required',
             'email' => 'required',
             'image' => 'required',
-            'googleId' => 'required',
+            'google_id' => 'required',
         ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user)
+        {
+            $userData['email'] = $request->email;
+            $userData['name'] = $request->name;
+            $userData['googele_id'] = $request->google_id;
+            $userData['image'] = $request->image;
+            $userData['password'] = bcrypt('$request->google_id');
+        
+            $user = User::create();
+        }
+        else
+        {
+            echo 2;
+        }
 
         
     }
